7ef0d418c109ed8fbef9988d018c06c6f0aa3bdd
kbkor <kbkorotkov@gmail.com>
2024-12-20 14:04:24 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using KorotkovUP02.viewModel;
+
+namespace KorotkovUP02
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        Navigation mainVM = new Navigation();
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // при запуске
+            base.OnStartup(e);
+            new MainWindow() { DataContext = mainVM }.Show();
+        }
+    }
+}
